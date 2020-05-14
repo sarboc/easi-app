12fb98cedc48c45278ddfe49791662f17e161673
Sara Bocciardi <sara@truss.works>
2020-05-13 22:08:44 -0700

    WIP 2

--- src/views/Sandbox/index.tsx
+++ src/views/Sandbox/index.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import Header from 'components/Header';
-import UpcomingActions from 'components/shared/UpcomingActions';
-import { SecureRoute, useOktaAuth } from '@okta/okta-react';
+import { useOktaAuth } from '@okta/okta-react';
 import ActionBanner from '../../components/shared/ActionBanner/index';
 
 // This view can be deleted whenever we're ready
 // This is just a sandbox page for us to test things out
 
-const onButtonClick = (authState: any) => {
-  alert(`I WAS CLICKED! ${authState}`)
+const onButtonClick = (authState: any, authService: any) => {
+  const tokenManager = authService.getTokenManager();
+  tokenManager.get('idToken').then((token: any) => {
+    console.log('token', token)
+  })
+  console.log('authState', authState);
+  alert(`I WAS CLICKED!`);
+  return null
 }
 
 const Sandbox = () => {
   console.log(useOktaAuth);
-  console.log(SecureRoute);
 
-  // const foo: any = useOktaAuth()
-  const authState: string = "Hello"
-  // const { authState }: { authState: object } = useOktaAuth();
+  const { authState, authService }: { authState: object, authService: any } = useOktaAuth();
 
   return (
     <div>
       <Header />
       <div className="grid-container">
         <h1>Sandbox</h1>
-        <UpcomingActions timestamp="FAKE TIME">
           <ActionBanner
             title="thing"
             helpfulText="lots of helpful text"
             label="I am a button"
-            onClick={() => onButtonClick(authState)}
+            onClick={() => onButtonClick(authState, authService)}
           />
-        </UpcomingActions>
       </div>
     </div>
   );
