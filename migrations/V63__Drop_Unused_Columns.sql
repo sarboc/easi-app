fce98563e47929c7783f3a279bdbce5616616fd5
Sara Bocciardi <sara@truss.works>
2020-12-21 16:20:06 -0800

    Remove old columns

---
+++ migrations/V63__Drop_Unused_Columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE system_intake DROP COLUMN isso;
+ALTER TABLE system_intake DROP COLUMN trb_collaborator;
+ALTER TABLE system_intake DROP COLUMN oit_security_collaborator;
+ALTER TABLE system_intake DROP COLUMN lcid_next_steps;
